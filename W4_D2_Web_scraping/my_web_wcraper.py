4c92535f73497dd3dc4c9a32a526d188fb65e2f3
Agnieszka000 <user.name>
2023-07-27 17:35:12 +0200

    Week 4 day excercise 2 added

---
+++ W4_D2_Web_scraping/my_web_wcraper.py
@@ -0,0 +1,11 @@
+# Stworzenie prostego programu do wyodrębniania danych ze strony internetowej.
+
+from bs4 import BeautifulSoup # I import BeautifulSoup.
+import requests # I import "requests" BS library.
+
+with open('/home/kali/Desktop/summer_of_code/W4_D2_Web_scraping/index.html') as html_file: # I read file and write it to a variable.
+    soup = BeautifulSoup(html_file, 'lxml') # I send the file to BeautifulSoup zmiennej "soup". I use "lxml" as a parser.
+
+tag1 = soup.find('ul') # I find a tag (<ul>)
+y = tag1.text # I extract a text from the tag.
+print(y)
