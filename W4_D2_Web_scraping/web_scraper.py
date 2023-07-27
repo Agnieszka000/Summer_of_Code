5bde875537a5e02f5cebeac0dff57a5dea1e3ae9
Agnieszka000 <user.name>
2023-07-27 16:16:46 +0200

    Week 4 day excercise modified

--- W4_D2_Web_scraping/web_scraper.py
+++ W4_D2_Web_scraping/web_scraper.py
@@ -5,10 +5,12 @@ with open('/home/kali/Desktop/summer_of_code/W4_D2_Web_scraping/simple.html') as
     soup = BeautifulSoup(html_file, 'lxml') # Przesylam plik do BeautifulSoup zmiennej "soup". Uzywam "lxml" jako parsera.
 # print(soup.prettify()) # Drukuje sformatowana wersje "soup".
 
-match = soup.title.text # Znajduje PIERWSZY tag <title> wraz z zawartoscia, oczyszczam go z samych tagow (.text) i zapisuje tekst do zmiennej "match".
-print(match)
+for article in soup.find_all('div', class_='article'):
 
-match2 = soup.find('div', class_= 'footer' ) # Zeby znalezc wszystkie dane tagi, wykorzystuje .find, a w nawiasie podaje atrybut szukanego taga.
-print(match2.text) # Drukuje info bez tagow.
+    print() #Pritns blank line between articles
 
-czas: 14:14
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.p.text
+    print(summary)
