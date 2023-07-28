db9985ba9071da3cd81f4a9a63f3e8b95ce37945
Agnieszka000 <user.name>
2023-07-28 10:07:35 +0200

    excercise correction

--- W4_D2_Web_scraping/my_web_wcraper.py
+++ W4_D2_Web_scraping/my_web_wcraper.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup # I import BeautifulSoup.
 import requests # I import "requests" BS library.
 
-with open('/home/kali/Desktop/summer_of_code/W4_D2_Web_scraping/index.html') as html_file: # I read file and write it to a variable.
+with open('index.html') as html_file: # I read file and write it to a variable. 
     soup = BeautifulSoup(html_file, 'lxml') # I send the file to BeautifulSoup zmiennej "soup". I use "lxml" as a parser.
 
 tag1 = soup.find('ul') # I find a tag (<ul>)
