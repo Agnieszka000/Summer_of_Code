86061404f03887b0294a07cba32422bf467e33c3
Agnieszka000 <dluska.agnieszka@gmail.com>
2023-08-04 01:12:55 +0200

    week5 day2 javascript exercise added

--- W5_D2_JavaScript/ex1.js
+++ W5_D2_JavaScript/ex1.js
@@ -1,4 +1,4 @@
-/* Spróbuj napisać kod, który w konsoli wyświetli liczby od 1 do 20, ale za każdym razem, gdy liczba jest podzielna przez 5 wyświetli dodatkowo napis "Hurra!" */
+/* Napisz kod, który w konsoli wyświetli liczby od 1 do 20, ale za każdym razem, gdy liczba jest podzielna przez 5 wyświetli dodatkowo napis "Hurra!" */
 
 for (let number = 1; number <= 20; number++) {
     if (number%5 == 0) {
