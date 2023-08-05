79fe733ee06026cb08d995c20f23d9bb7cdc4c56
Agnieszka000 <dluska.agnieszka@gmail.com>
2023-08-05 21:35:33 +0200

    week5 day3 javascript exercise2 added

---
+++ W5_D3_JavaScript/ex2.js
@@ -0,0 +1,5 @@
+/*Make a variable test set it equal to a prompt box, and type "Hi!" in it (without the quotes) when it pops up. Note: your pop-up blocker must not be enabled.*/
+
+let test = prompt("Hi!")
+
+console.log(test);
