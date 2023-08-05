3f55b8764b6d8fb43792dc72b796824758005c7f
Agnieszka000 <dluska.agnieszka@gmail.com>
2023-08-05 22:35:37 +0200

    week5 day3 javascript exercise3 added

---
+++ W5_D3_JavaScript/ex3.js
@@ -0,0 +1,48 @@
+/*Napisz skrypt, który wylosuje dowolną liczbę całkowitą od zera do 10 a następnie prosi użytkownika o jej zgadniecie, ale maksymalnie 5 razy
+
+Gdy program działa, rozszerz go np. o podawanie informacji za którym razem udało się zgadnąć lub o wskazówki typu 
+”Podana przez ciebie liczba jest większa/mniejsza od wylosowanej”*/
+
+
+let number = parseInt(Math.random()*10);
+let userNumber = parseInt(prompt("Wylosowalam liczbe. Zgadnij ja. Podaj od 0 do 10:"));
+
+function inf() {
+    if (userNumber < number) {
+        alert('Podana liczna jest za mala');
+    } else {
+        alert('Podana liczba jest za duza.');
+    }
+}
+
+function repeatNumber() {
+  let n = parseInt(4);
+  while (n > 0) {
+  userNumber = parseInt(prompt('Niestety nie zgadlas. Sprobuj jeszcze raz:'));
+    if (userNumber === number) {
+      alert('Brawo. Zgadlas!');
+      console.log('Brawo!');
+      break;
+      } else {
+      inf();
+      n = (n - 1);
+      if (n === 0) {
+        alert('Niestety to koniec gry.')
+        console.log('GAME OVER')
+      }
+    }
+  }
+}
+
+
+function guessNumber() {
+  if (userNumber !== number) {
+    inf();
+    repeatNumber();
+    } else {
+    alert('Brawo. Zgadlas!');
+    console.log('Brawo. Zgadlas!');
+  }
+}
+
+guessNumber();
