ab1c84a0fdbac6c3fdb6eaeee3dd94e3aa137a76
Agnieszka000 <dluska.agnieszka@gmail.com>
2023-08-09 14:15:22 +0200

    week6 day3 api fetch exercise1 updated

--- W6_D3_API_fetch/ex1.js
+++ W6_D3_API_fetch/ex1.js
@@ -2,4 +2,12 @@
 
 fetch('https://reqres.in/api/users?page=1')
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(data => {
+        let people = data.data;
+        for (let i=0; i < people.length; i++) {
+        console.log('IMIE: ' + people[i].first_name);
+        console.log('NAZWISKO: ' + people[i].last_name);
+        console.log('EMAIL: ' + people[i].email);
+        console.log("----");
+        }
+    })
